803ec8509b026f7a0dc40c4564b71e973cd2daf5
Jim Balhoff <balhoff@nescent.org>
2011-02-18 15:32:14 -0500

    Initial commit.

---
+++ src/org/hymao/mx/QueryCharactersForConcept.java
@@ -0,0 +1,19 @@
+package org.hymao.mx;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+
+public class QueryCharactersForConcept {
+
+    public QueryCharactersForConcept(OWLOntology ontology, String conceptURI) {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
